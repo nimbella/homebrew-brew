a3e64e21a69b5b51edfa85ef1f638215cfbd57b2
crai <dev.chandan.rai@gmail.com>
2021-07-27 00:11:24 +0530

    update sha

--- Formula/nimbella.rb
+++ Formula/nimbella.rb
@@ -3,7 +3,7 @@ class Nimbella < Formula
   desc "Everything you need to get started with Nimbella"
   homepage "https://docs.nimbella.com/command-summary"
   url "https://apigcp.nimbella.io/downloads/nim/nim-v1.16.0/nim-v1.16.0.tar.gz"
-  sha256 "42f82e02611af5b61a83b14ca5f8dc8bff5285ca9182cf4742e1d188cb8bf905"
+  sha256 "9ded04e0ebc3b0de2ef53c84ccf9073018c220a5fbabcc68298b83a507b5bd11"
   depends_on "nimbella/brew/nimbella-node" => "14.4.0"
 
   def install
--- README.md
+++ README.md
@@ -3,6 +3,7 @@ Nimbella Homebrew Tap
 
 Install the Nimbella CLI with
 
-    $ brew install nimbella/brew/nimbella-cli
+    $ brew install nimbella/brew/nimbella
+    
 
 For more information, visit https://docs.nimbella.com
