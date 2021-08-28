7860e59b4b39b699d4dd7a95f1575eb8571aa519
crai <dev.chandan.rai@gmail.com>
2021-08-28 19:49:42 +0530

    remove node installation

--- Formula/nim.rb
+++ Formula/nim.rb
@@ -3,7 +3,6 @@ class Nim < Formula
   homepage "https://docs.nimbella.com/command-summary"
   url "https://apigcp.nimbella.io/downloads/nim/nim-v1.16.0/nim-v1.16.0.tar.gz"
   sha256 "9ded04e0ebc3b0de2ef53c84ccf9073018c220a5fbabcc68298b83a507b5bd11"
-  depends_on "nimbella/brew/nim-node" => "14.4.0"
 
   def install
     inreplace "bin/nim", /^CLIENT_HOME=/, "export NIMBELLA_OCLIF_CLIENT_HOME=#{lib/"client"}\nCLIENT_HOME="
