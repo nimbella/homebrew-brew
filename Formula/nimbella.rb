f7121e7e7c9558901b7e81d7320519a9bf5f4def
crai <dev.chandan.rai@gmail.com>
2021-08-30 20:42:06 +0530

    nimbella v1.16.0

--- Formula/nimbella.rb
+++ Formula/nimbella.rb
@@ -3,21 +3,20 @@
 # Please update the template instead:
 # https://github.com/nimbella/nimbella-cli/blob/master/release/homebrew/templates/nimbella.rb
 class Nimbella < Formula
-  desc "Everything you need to get started with Nimbella"
-  homepage "https://docs.nimbella.com/command-summary"
-  license "Apache-2.0"
-  url "https://apigcp.nimbella.io/downloads/nim/nim-v1.16.0/nim-v1.16.0.tar.gz"
-  sha256 "42f82e02611af5b61a83b14ca5f8dc8bff5285ca9182cf4742e1d188cb8bf905"
-  depends_on "nimbella/brew/nimbella-node" => "14.4.0"
-
-  def install
-    inreplace "bin/nim", "\"$DIR/node\"", Formula["nimbella-node"].opt_bin/"node"
-    libexec.install Dir["*"]
-    bin.install_symlink libexec/"bin/nim"
-  end
-
-  test do
-    system bin/"nim", "version"
-  end
+    desc "Everything you need to get started with Nimbella"
+    homepage "https://docs.nimbella.com/command-summary"
+    license "Apache-2.0"
+    url "https://apigcp.nimbella.io/downloads/nim/nim-v1.16.0/nim-v1.16.0.tar.gz"
+    sha256 "42f82e02611af5b61a83b14ca5f8dc8bff5285ca9182cf4742e1d188cb8bf905"
+    depends_on "nimbella/brew/nimbella-node" => "14.4.0"
+  
+    def install
+      inreplace "bin/nim", "\"$DIR/node\"", Formula["nimbella-node"].opt_bin/"node"
+      libexec.install Dir["*"]
+      bin.install_symlink libexec/"bin/nim"
+    end
+  
+    test do
+      system bin/"nim", "version"
+    end
 end
-
